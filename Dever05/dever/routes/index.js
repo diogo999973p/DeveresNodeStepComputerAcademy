3aafd6e5c2672426ab40fb97ec5d14b0f02f06c3
diogo999973p <diogogbrandao@msn.com>
2021-07-24 16:02:14 -0300

    dever5

--- Dever05/dever/routes/index.js
+++ Dever05/dever/routes/index.js
@@ -3,7 +3,15 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+  res.render('index');
+});
+
+router.get('/quartos', function(req, res, next) {
+  res.render('quartos');
+});
+
+router.get('/reservas', function(req, res, next) {
+  res.render('reservas');
 });
 
 module.exports = router;
